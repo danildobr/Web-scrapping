c6288dbfe3c0f3a30d57e12ad7bd61fe299164ea
Danil_Dobr <danil.bore@mail.ru>
2025-04-04 18:39:55 +0500

    эдобавил коментарии

--- Web-scrapping.py
+++ Web-scrapping.py
@@ -20,36 +20,44 @@ from datetime import datetime
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
 def website_article():
+    '''Получаем html страницу сайта '''
     respons = requests.get('https://habr.com/ru/articles/', headers=Headers(browser='chrom', os='win').generate())
+    # делаем запрос на сайт 
     soup = bs4.BeautifulSoup(respons.text, features='lxml' )
+    # помещаем сайт в парссер
     article_list = soup.find_all('article', class_= 'tm-articles-list__item')
+    # собираем список статей
     return article_list
+    # возвращаем список статей  
 
 def website_parsing(article_list):
+    '''Парсим нужную информацияю и формируем вывод'''
     for article in article_list:
-        article_url = ('https://habr.com/ru/articles/' + article.get('id'))
-        
-        article_header = article.find('h2').text
-        
+        article_url = ('https://habr.com/ru/articles/' + article.get('id')) 
+        # формируем url
+        article_header = article.find('h2').text 
+        # находим заголовок
         article_time = article.find('time')['datetime']
+        # находим дату 
         parsed_date = datetime.strptime(article_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-        formatted_date = parsed_date.strftime('%d.%m.%Y %H:%M')
-        
+        formatted_date = parsed_date.strftime('%d.%m.%Y %H:%M')  
+        # формируем красиво дату 
         preview_div = article.find('div', class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
         if preview_div:
             preview_text = preview_div.text.strip()
         else: ""
-        
+        # находим preview текст
         has_keyword = any(
         keyword.lower() in article_header.lower() or 
         keyword.lower() in preview_text.lower()
         for keyword in KEYWORDS)
-    
+        # проверяем находиться наши ключевые слова в заголовке и preview тексте 
         if article_header and has_keyword:
             print(f'''Дата публикации: {formatted_date}, Заголовок: "{article_header}", Ссылка: {article_url}''')
+        # формируем строку 
         
 if __name__ == '__main__':
     website_parsing(website_article())
-    
+    # вызываем функцию 
 
 
